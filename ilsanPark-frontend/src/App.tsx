abbda3f3567a511d75c61d34b7a194ab3f111a95
jm4293 <dlwoals4293@gmail.com>
2024-02-14 23:25:05 +0900

    front - config emotion/react

--- ilsanPark-frontend/src/App.tsx
+++ ilsanPark-frontend/src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { buttonBlue } from './styles/button/button.style';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          가능함?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div css={buttonBlue}>dd</div>
+    </>
   );
 }
 
