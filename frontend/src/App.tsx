356fae39fdd0c435fa8af3dd1ade01df7d23ab04
jm4293 <dlwoals4293@gmail.com>
2024-02-21 23:49:30 +0900

    frontend - url navigate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Layout } from './components/layout/layout';
 import './styles/common.css';
 import './styles/font/font.css';
+import { BrowserRouter } from 'react-router-dom';
+import { RootRouters } from './routers/rootRouters';
 
 function App() {
   return (
     <>
-      <Layout />
+      <BrowserRouter>
+        {/*<Layout />*/}
+        <RootRouters />
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -6,28 +6,38 @@ import { FaCar } from 'react-icons/fa6';
 import { BsThreeDots } from 'react-icons/bs';
 
 import { footer_item_style, footer_style } from '../../styles/layout/layout_style';
+import { useRouterStore } from '../../data/zustand/router.store';
+import { useNavigate } from 'react-router-dom';
 
 export const Footer = () => {
+  const navigate = useNavigate();
+  const BASE_URL = useRouterStore((state) => state.base_url);
+
+  const onClickHandler = (url: string) => {
+    console.log('ur', url);
+    navigate('/' + BASE_URL + url);
+  };
+
   return (
     <>
       <div css={footer_style}>
-        <div css={footer_item_style}>
+        <div css={footer_item_style} onClick={() => onClickHandler('/home')}>
           <BiSolidHome size="2rem" />
           <div>홈</div>
         </div>
-        <div css={footer_item_style}>
+        <div css={footer_item_style} onClick={() => onClickHandler('/charging')}>
           <RiBattery2ChargeFill size="2rem" />
           <div>충전소 찾기</div>
         </div>
-        <div css={footer_item_style}>
+        <div css={footer_item_style} onClick={() => onClickHandler('/wallet')}>
           <IoIosWallet size="2rem" />
           <div>카드지갑</div>
         </div>
-        <div css={footer_item_style}>
+        <div css={footer_item_style} onClick={() => onClickHandler('/mycar')}>
           <FaCar size="2rem" />
           <div>내차관리</div>
         </div>
-        <div css={footer_item_style}>
+        <div css={footer_item_style} onClick={() => onClickHandler('/setting')}>
           <BsThreeDots size="2rem" />
           <div>더보기</div>
         </div>
